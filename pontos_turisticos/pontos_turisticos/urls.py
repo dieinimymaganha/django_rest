aa71875bc290126adf1af32fe208e75f5413ca73
Dieinimy Maganha <dieinimy@gmail.com>
2019-11-20 19:49:48 -0300

    Corrigindo nome de atração

--- pontos_turisticos/pontos_turisticos/urls.py
+++ pontos_turisticos/pontos_turisticos/urls.py
@@ -18,12 +18,12 @@ from django.urls import path
 from django.conf.urls import include
 from rest_framework import routers
 from core.api.viewsets import PontoTuristicoViewSet
-from atracoes.api.viewsets import AtracoesViewSet
+from atracoes.api.viewsets import AtracaoViewSet
 from enderecos.api.viewsets import EnderecoViewSet
 
 router = routers.DefaultRouter()
 router.register(r'pontoturistico', PontoTuristicoViewSet)
-router.register(r'atracoes', AtracoesViewSet)
+router.register(r'atracoes', AtracaoViewSet)
 router.register(r'enderecos', EnderecoViewSet)
 
 urlpatterns = [
