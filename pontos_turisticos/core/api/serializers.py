420650b5d950194fbe98eb746a670cac8b2833bc
Dieinimy Maganha <dieinimy@gmail.com>
2019-11-20 21:05:11 -0300

    Incluindo uploads de fotos em core e atracoes

--- pontos_turisticos/core/api/serializers.py
+++ pontos_turisticos/core/api/serializers.py
@@ -6,4 +6,4 @@ class PontoTuristicoSerializer(ModelSerializer):
 
     class Meta:
         model = PontoTuristico
-        fields = ('id', 'nome', 'descricao', 'aprovado')
+        fields = ('id', 'nome', 'descricao', 'aprovado', 'foto')
