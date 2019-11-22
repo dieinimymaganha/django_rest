00263e701af2650961e438da60655d0dff27aa73
Dieinimy Maganha <dieinimy@gmail.com>
2019-11-22 20:22:32 -0300

    Usando o property para exibir dados

--- pontos_turisticos/core/api/serializers.py
+++ pontos_turisticos/core/api/serializers.py
@@ -1,15 +1,23 @@
+from rest_framework.fields import SerializerMethodField
 from rest_framework.serializers import ModelSerializer
 from core.models import PontoTuristico
 from atracoes.api.serializers import AtracaoSerializer
 from enderecos.api.serializers import EnderecoSerializer
 
 
-class PontoTuristicoSerializer(ModelSerializer):
 
+
+
+class PontoTuristicoSerializer(ModelSerializer):
     atracaoes = AtracaoSerializer(many=True)
     endereco = EnderecoSerializer()
+    descricao_completa = SerializerMethodField()
+
 
     class Meta:
         model = PontoTuristico
         fields = ('id', 'nome', 'descricao', 'aprovado', 'foto',
-                  'atracaoes', 'comentarios', 'avaliacoes', 'endereco')
+                  'atracaoes', 'comentarios', 'avaliacoes', 'endereco', 'descricao_completa', 'descricao_completa2')
+
+    def get_descricao_completa(self, obj):
+        return '%s - %s' % (obj.nome, obj.descricao)
